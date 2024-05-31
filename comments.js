ec88d783a18dbf43d499af88bc0e244dfd6e706e
离离 <mar@mail.ustc.edu.cn>
2024-05-31 08:18:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,40 +4,22 @@ var fs = require('fs');
 var url = require('url');
 var path = require('path');
 
-var server = http.createServer(function(req, res) {
-	var urlObj = url.parse(req.url, true);
-	var pathname = urlObj.pathname;
-	if (pathname == '/') {
-		pathname = '/index.html';
-	}
-	var extname = path.extname(pathname);
-	if (pathname != '/favicon.ico') {
-		fs.readFile('./' + pathname, function(err, data) {
-			if (err) {
-				res.writeHead(404, {
-					'Content-Type': 'text/html;charset=UTF-8'
-				});
-				res.end('404,Page Not Found.');
-			}
-			if (extname == '.html') {
-				res.writeHead(200, {
-					'Content-Type': 'text/html;charset=UTF-8'
-				});
-				res.end(data);
-			}
-			if (extname == '.css') {
-				res.writeHead(200, {
-					'Content-Type': 'text/css'
-				});
-				res.end(data);
-			}
-			if (extname == '.js') {
-				res.writeHead(200, {
-					'Content-Type': 'application/x-javascript'
-				});
-				res.end(data);
-			}
-		})
-	}
-})
-server.listen(3000);
+//create server
+var server = http.createServer(function (req, res) {
+    var pathname = url.parse(req.url).pathname;
+    var filePath = path.join(__dirname, pathname);
+
+    fs.readFile(filePath, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('404 Not Found');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(3000, function () {
+    console.log('Server is running on port 3000');
+});
